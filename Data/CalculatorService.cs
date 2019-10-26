57f7ebb3d74e6d5f282fd96c38244e5b50954dfb
Iraz Irfan <iraz@mislbd.com>
2019-10-26 08:57:50 +0600

    Project Init

---
+++ Data/CalculatorService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShowCase.Data
+{
+    public class CalculatorService
+    {
+        public Task<double> Calculate(double a, double b)
+        {
+            return Task.FromResult(a * b);
+        }
+    }
+}
